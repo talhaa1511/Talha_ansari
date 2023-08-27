3059941f18285ced78f01ab1fd8d0b5f7a3f50cb
Talha Ansari <bilqeesjamal2022@gmail.com>
2023-08-27 16:23:00 +0500

    commit 13

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,6 @@
 import React from "react"
 import { useState } from "react"
 import { Link } from "react-scroll/modules"
-import { usePathname } from "next/navigation"
 import { useTheme } from "next-themes"
 import { RiMoonFill, RiSunLine } from "react-icons/ri"
 import { IoMdMenu, IoMdClose } from "react-icons/io"
